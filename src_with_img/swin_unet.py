76c409ebd63435cabd9958471dd7229fe96f12e3
hisakot <sakigake.apple@gmail.com>
2025-03-15 00:40:52 +0900

    apply full img size

--- src_with_img/swin_unet.py
+++ src_with_img/swin_unet.py
@@ -14,18 +14,18 @@ class SwinUNet(nn.Module):
         self.enc_out_channels = [96, 192, 384, 768] # output of Swin-Tiny channels
 
         # Decoder (Unet style upsampling with skip connections)
-        self.upconv3 = self._upsample_block(self.enc_out_channels[3], self.enc_out_channels[2])
-        self.upconv2 = self._upsample_block(self.enc_out_channels[2], self.enc_out_channels[1])
-        self.upconv1 = self._upsample_block(self.enc_out_channels[1], self.enc_out_channels[0])
-        self.final_conv = nn.Conv2d(self.enc_out_channels[0], num_classes, kernel_size=1)
+        self.upconv5 = self._upsample_block(768, 384)
+        self.upconv4 = self._upsample_block(384, 192)
+        self.upconv3 = self._upsample_block(192, 96)
+        self.upconv2 = self._upsample_block(96, 48)
+        self.upconv1 = self._upsample_block(48, 24)
+        self.final_conv = nn.Conv2d(24, num_classes, kernel_size=1)
 
     def _upsample_block(self, in_ch, out_ch):
         return nn.Sequential(
+                nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True))
                 nn.Conv2d(in_ch, out_ch, kernel_size=3, padding=1),
                 nn.ReLU(inplace=True),
-                nn.Conv2d(out_ch, out_ch, kernel_size=3, padding=1),
-                nn.ReLU(inplace=True),
-                nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True))
 
     def forward(self, x):
         B, C, H, W = x.shape
@@ -34,12 +34,14 @@ class SwinUNet(nn.Module):
 
         #Reshape (L = H' * W') -> (B, C, H', W')
         H_new, W_new = H // 32, W // 32 # feature map size of SwinTransformer
-        features = features.permute(0, 2, 1).contiguous().view(B, -1, H_new, W_new) # (B, C, H', W')
+        features = features.permute(0, 2, 1).contiguous().view(B, 768, H_new, W_new) # (B, C, H', W')
 
         # Decoder Path with Skip Connection
-        d3 = self.upconv3(features) # B, 384, Hx2, Wx2)
-        d2 = self.upconv2(d3) # B, 192, Hx4, Wx4)
-        d1 = self.upconv1(d2) # B, 96, Hx8, Wx8)
-        out = self.final_conv(d1) # B, 1, Hx8, Wx8)
+        d5 = self.upconv5(features) # (120x240)
+        d4 = self.upconv4(d5) # (240x480)
+        d3 = self.upconv3(d4) # (480x960)
+        d2 = self.upconv2(d3) # (960x1920)
+        d1 = self.upconv1(d2) # (1920x3840)
+        out = self.final_conv(d1) # (B, 1, 1920, 3840)
 
         return out
