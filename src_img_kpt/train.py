6aaeaca9717609fb7c29ca9d704b4811b2f00655
hisakot <sakigake.apple@gmail.com>
2025-04-02 04:34:22 +0900

    save name

--- src_img_kpt/train.py
+++ src_img_kpt/train.py
@@ -254,7 +254,7 @@ def main():
                             "train_loss_list" : train_loss_list,
                             "train_loss_list" : train_loss_list,
                             "val_loss_list" : val_loss_list,
-                            }, "save_models/img_pjae_best.pth")
+                            }, "save_models/img_gazecone_pjae_best.pth")
             else:
                 early_stopping[2] += 1
                 if early_stopping[2] == early_stopping[1]:
