0c1031679f0b6fc842ccbcef27662c5f0efd6162
hisakot <sakigake.apple@gmail.com>
2025-05-08 11:30:23 +0900

    repair

--- src_img_kpt/eval_result.py
+++ src_img_kpt/eval_result.py
@@ -10,25 +10,31 @@ from tqdm import tqdm
 
 gt_paths = glob.glob("data/test/gt_heatmap_1ch/*/*.png")
 gt_paths.sort()
-pred_paths = glob.glob("data/test/pred/gazecone_clone_kpt_lined_cossim/*.png")
+pred_paths = glob.glob("data/test/pred/gazecone_close_kpt_lined_cossim/*.png")
 pred_paths.sort()
 print(len(gt_paths), len(pred_paths))
 
 x, y, xy = 0, 0, 0
 for i, gt_path in tqdm(enumerate(gt_paths), total=len(gt_paths)):
-    gt = cv2.imread(gt_path)
+    gt = cv2.imread(gt_path, 0)
     gt = cv2.resize(gt, (640, 320))
+    gt = gt.astype(np.float64)
+    gt /= 255.
     gt_max = np.max(gt)
     gt_argmax = np.unravel_index(np.argmax(gt), gt.shape)
-    gt_flat = np.ravel(gt)
+    gt_flat = gt.reshape(-1)
+    gt_flat = np.where(gt_flat > 0, 1, 0)
 
     pred_path = pred_paths[i]
-    pred = cv2.imread(pred_path)
+    pred = cv2.imread(pred_path, 0)
     pred = cv2.resize(pred, (640, 320))
+    pred = pred.astype(np.float64)
+    pred /= 255.
     pred_max = np.max(pred)
     pred_argmax = np.unravel_index(np.argmax(pred), pred.shape)
-    pred_flat = np.ravel(pred)
+    pred_flat = pred.reshape(-1)
     print(pred.shape, pred_flat.shape)
+    print(pred_max, gt_max)
 
     dist_x = abs(pred_argmax[0] - gt_argmax[0])
     dist_y = abs(pred_argmax[1] - gt_argmax[1])
@@ -41,7 +47,7 @@ for i, gt_path in tqdm(enumerate(gt_paths), total=len(gt_paths)):
     roc_auc = auc(fpr, tpr)
 
     plt.figure(figsize=(8, 6))
-    plt.plot(fpr, tpr, color="darkorange", lw=2, label=f"ROC curve (area = {roc_curve:.2f})")
+    plt.plot(fpr, tpr, color="darkorange", lw=2, label=f'ROC curve (area = {roc_curve:.2f})')
     plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
