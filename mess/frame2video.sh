8db9feaa03694597fd95365b3caa50eed7ab4688
hisakot <sakigake.apple@gmail.com>
2025-06-05 05:14:10 +0900

    file path

--- mess/frame2video.sh
+++ mess/frame2video.sh
@@ -1,1 +1,1 @@
-ffmpeg -r 1 -i data/test/pred/gaze_mult_selected_augmentation_135/%06d.png -vcodec libx264 -pix_fmt yuv420p -r 1 data/test/selected_augmentation_135.mp4
+ffmpeg -r 1 -i data/test/pred/gaze_mult_allaround_augmentation_45/%06d.png -vcodec libx264 -pix_fmt yuv420p -r 1 data/test/allaround_augmentation_45.mp4
