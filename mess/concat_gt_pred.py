e895719c65319b118d41dbd8dff6133a368e9e5c
hisakot <sakigake.apple@gmail.com>
2025-06-04 12:17:39 +0900

    file path

--- mess/concat_gt_pred.py
+++ mess/concat_gt_pred.py
@@ -6,7 +6,7 @@ import os
 
 gt_paths = glob.glob("data/test/gt_heatmap_1ch/*/*.png")
 gt_paths.sort()
-pred_paths = glob.glob("data/test/pred/gaze_mult_selected_augmentation/*.png")
+pred_paths = glob.glob("data/test/pred/gaze_mult_selected_augmentation_no_rotate/*.png")
 pred_paths.sort()
 img_paths = glob.glob("data/test/frames/*/*.png")
 img_paths.sort()
