32e0ecbf0c910cfb9802463f2107dda14f67bd2b
hisakot <sakigake.apple@gmail.com>
2025-07-24 00:34:30 +0900

    apply PJAE result

--- mess/frame2video.sh
+++ mess/frame2video.sh
@@ -1,1 +1,2 @@
-ffmpeg -r 1 -i data/test/pred/result/%06d.png -vcodec libx264 -pix_fmt yuv420p -r 1 data/test/transGan_cossim.mp4
+# ffmpeg -r 1 -i data/test/pred/result/%06d.png -vcodec libx264 -pix_fmt yuv420p -r 1 data/test/transGan_cossim.mp4
+ffmpeg -r 1 -i ~/Lab/PJAE-ICCV2023-UE/results/Medical/with_ann_action_50epoch/final_jo_att_superimposed/test_ds_005_000001_%06d_final_jo_att_superimposed.png -vcodec libx264 -pix_fmt yuv420p -r 1 data/test/pred/PJAE_with_ann_50epoch.mp4
