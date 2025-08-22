cd1781581a67d6e4a3dbdb0bed2008e637fac749
hisakot <sakigake.apple@gmail.com>
2025-08-22 16:24:17 +0900

    video

--- mess/frame2video.sh
+++ mess/frame2video.sh
@@ -1,2 +1,2 @@
-ffmpeg -r 1 -i data/test/pred/0731/swinunet_no_skipconnection_colormap/%06d.png -vcodec libx264 -pix_fmt yuv420p -r 1 data/test/pred/swinunet_no_skipconnection_colormap.mp4
+ffmpeg -r 1 -i data/test/pred/result/%06d.png -vcodec libx264 -pix_fmt yuv420p -r 1 data/test/pred/SwinUnetLSTM_seq2.mp4
 # ffmpeg -r 1 -i ~/Lab/PJAE-ICCV2023-UE/results/Medical/with_ann_action_50epoch/final_jo_att_superimposed/test_ds_005_000001_%06d_final_jo_att_superimposed.png -vcodec libx264 -pix_fmt yuv420p -r 1 data/test/pred/PJAE_with_ann_50epoch.mp4
